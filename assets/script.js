9b1abe384f58899872f573389404537eaa89e359
John Doe <johndoe@example.com>
2023-04-30 02:45:54 +0200

    Ajout du slide et des bullets

--- assets/script.js
+++ assets/script.js
@@ -15,4 +15,37 @@ const slides = [
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+];
+
+const arrowLeft = document.querySelector('.arrow_left');
+const arrowRight = document.querySelector('.arrow_right');
+
+arrowLeft.addEventListener('click', slideprecedent);
+arrowRight.addEventListener('click', slidesuivant);
+
+
+const bannerImg = document.querySelector('.banner-img');
+const tagLine = document.querySelector('#banner p');
+
+let currentSlide = 0;
+
+function slide() {
+    bannerImg.src = './assets/images/slideshow/' + slides[currentSlide].image;
+    tagLine.innerHTML = slides[currentSlide].tagLine;
+}
+
+function slidesuivant() {
+    currentSlide++;
+    slide();
+}
+
+function slideprecedent() {
+    currentSlide--;   
+	slide();
+}
+
+const dots = document.querySelector('.dots');
+
+
+
+
